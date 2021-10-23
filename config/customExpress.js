a5661dc3ab7c6e3dafbee14ffc0babdccf1f0d40
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-23 19:13:32 -0300

    Features
    Novas funções

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require('express') 
 const consign = require('consign')
+const bodyPost = require('body-parser')
 
 module.exports = () => {
     const app = express()
+
+    app.use(bodyPost.urlencoded({extended: true}))
+    app.use(bodyPost.json())
 
     consign()
         .include('controllers')
