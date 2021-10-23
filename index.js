deb9d07301d18da75d0af1047a64cca0bf34b891
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-23 20:11:47 -0300

    adicionando funções
    features

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
 
-app.listen(5000, () => console.log('servidor rodando na porta 5000'))
+        const app = customExpress()
+
+        app.listen(5000, () => console.log('servidor rodando na porta 5000'))
+    }
+})
